e94e519a13a5cdfd689cca9d6d6aee9aeca0a124
BambusTM <81088539+BambusTM@users.noreply.github.com>
2024-08-05 14:01:37 +0200

    now removing emojis from tokens

--- tokenize_reviews.py
+++ tokenize_reviews.py
@@ -4,6 +4,7 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import string
+import emoji
 
 token_list = []
 
@@ -22,8 +23,11 @@ def tokenize(data):
             comment_title: str = review.get('comment_title')
             comment_content: str = review.get('comment_content')
 
-            title_tokens = word_tokenize(comment_title)
-            comment_tokens = word_tokenize(comment_content)
+            no_emoji_title = remove_emojis(comment_title)
+            no_emoji_comment = remove_emojis(comment_content)
+
+            title_tokens = word_tokenize(no_emoji_title)
+            comment_tokens = word_tokenize(no_emoji_comment)
 
             stop_title_tokens = remove_stop_words(title_tokens)
             stop_title_tokens = remove_stop_words(comment_tokens)
@@ -36,7 +40,7 @@ def tokenize(data):
 
     return token_list
 
-def remove_stop_words(word_tokens):
+def remove_stop_words(input):
     nltk.download('stopwords')
     
     stop_words_de = set(stopwords.words('german'))
@@ -45,9 +49,17 @@ def remove_stop_words(word_tokens):
     stop_words = stop_words_de.union(stop_words_fr).union(stop_words_en)
     punctuation = set(string.punctuation)
     
-    filtered = [w for w in word_tokens if w.lower() not in stop_words and w not in punctuation]
+    filtered = [w for w in input if w.lower() not in stop_words and w not in punctuation]
 
     return filtered
+
+def remove_emojis(input):
+    allchars = [str for str in input]
+    emoji_list = [c for c in allchars if c in emoji.EMOJI_DATA]
+    clean_text = ' '.join([str for str in input.split() if not any(i in str for i in emoji_list)])
+        
+    return clean_text
+    
 
 def write_json(input):
     formatted_list = [
